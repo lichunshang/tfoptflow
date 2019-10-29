33ae7face1b54217d65558a57fcf252de371f152
Chunshang Li <jack.chunshang.li@gmail.com>
2019-10-29 15:48:53 -0400

    made the package a loadable module

---
+++ tfoptflow/__init__.py
@@ -0,0 +1,10 @@
+# from model_pwcnet import ModelPWCNet, _DEFAULT_PWCNET_TEST_OPTIONS
+import model_pwcnet
+import model_base
+import optflow
+import losses
+import logger
+import multi_gpus
+import core_warp
+import core_costvol
+import utils
